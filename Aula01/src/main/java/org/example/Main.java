5fb89732d28fbc304db5f394011d8055caf94c78
Aluno do Inatel <aluno@local.inatel.br>
2023-02-14 11:24:13 -0300

    Primenro código de POO

---
+++ Aula01/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        String nome = "Bruno Vinícius";
+        System.out.println(nome);
+
+
+
+
+
+
+    }
+}
